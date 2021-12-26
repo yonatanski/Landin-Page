46db7748ee7712d1fa27072d043c3edbce7b02d2
yonatanski <phpcodeyg@gmail.com>
2021-12-26 10:26:21 +0100

    alignemnt change

--- landing_page/src/Components/navbar/NavBar.jsx
+++ landing_page/src/Components/navbar/NavBar.jsx
@@ -1,59 +1,56 @@
-import React, {useState} from "react"
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
-import "./NavBar.css"
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from "../../assets/logo.svg"
+import './NavBar.css';
 
-const NavBar = () => {
 
-//    to reuse this html
-const Menu=()=>(
-    <>
-    <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">What iS GPT3</a></p>
-    <p><a href="#possiblity">Open AI</a></p>
-    <p><a href="#features">Case Studies</a></p>
-    <p><a href="#blog">Library</a></p>
-    </>
-)
 
-   const [toogleMenu,setToogleMenu] =useState(false)     
- 
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-            <img src={logo} alt="logo" />
+          <img src={logo} />
         </div>
         <div className="gpt3__navbar-links_container">
-       <Menu/>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
-      {/*  */}
-    <div className="gpt3__navbar-sign">
+      <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
         </div>
-        <div className="gpt3__navbar-menu">
-            {
-              toogleMenu ? <RiCloseLine color="#fff" size={27} onClick={()=>setToogleMenu(false)}/>
-              :<RiMenu3Line color="#fff" size={27} onClick={()=>setToogleMenu(true)}/>
-            }
-            {toogleMenu && (
-                <div  className="gpt3__navbar-menu_container scale-up-center">
-                    <div className="gpt3__navbar-menu_container-links ">
-                    <Menu/>
-                    <div className="gpt3__navbar-sign-menu_container-links-sign">
-                   <p>Sign in</p>
-                   <button type="button">Sign up</button>
-                   </div>
-                    </div>
-                    
-                 </div>
-            )}
-        </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default Navbar;
+import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
